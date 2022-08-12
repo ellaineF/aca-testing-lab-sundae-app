b5e8f9f481a4215610d57e54638224143cedbb76
Ellaine <ellaineellie19@gmail.com>
2022-08-12 12:50:46 +0200

    Add Test File

---
+++ src/tests/orderPhase.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+test('' , () => {
+    render(<App />);
+})
